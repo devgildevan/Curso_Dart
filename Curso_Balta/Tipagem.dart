71cf4e8ebdf718fe39231489010540659097c262
Gildevan <gildevan87654321@gmail.com>
2022-12-06 20:01:11 -0300

    Aprendendo dart

--- Curso_Balta/Tipagem.dart
+++ Curso_Balta/Tipagem.dart
@@ -1,1 +1,17 @@
-void main(List<String> args) {}
+import 'dart:io';
+
+main() {
+  bool condicao = true;
+
+  while (condicao) {
+    print("=========== Escreva um texto ==========");
+    String? texto = stdin.readLineSync();
+
+    if (texto == "sair") {
+      condicao = false;
+      print("Fim!");
+    } else {
+      print("Erro você digitou $texto");
+    }
+  }
+}
